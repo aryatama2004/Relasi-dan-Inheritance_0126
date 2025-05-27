823f062281ddc7d84ff5aca7d92096f7b2b10c0a
aryatama2004 <aryatama261@gmail.com>
2025-05-27 15:38:45 +0700

    menambahkan dua kelas dokter dan pasien

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 class dokter;
-class pasien { 
-    Public :
+class pasien {
+public :
     string nama;
     vector<dokter*> daftar_dokter;
     pasien(string pNama) :nama(pNama) {
@@ -12,6 +12,20 @@ class pasien {
     }
     void tambahDokter(dokter*);
     void cetakDokter();    
-
 };
+class dokter {
+public :
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    void tambahPasien(pasien*);
+    void cetakPasien();
+};   
 
