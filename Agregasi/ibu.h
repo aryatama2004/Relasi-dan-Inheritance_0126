e8345f0dd0ab29add9de32fe4869ba1694c8f362
aryatama2004 <aryatama261@gmail.com>
2025-05-27 15:16:07 +0700

    membuat class ibu dan daftar anak

---
+++ Agregasi/ibu.h
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include "anak.h"
+#define IBU_H
+
+using namespace std;
+
+
+class ibu
+{
+public:
+    string nama;
+    vector<anak *> daftar_anak;
+
+    ibu(string pNama) : nama(pNama)
+    {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu()
+    {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
+    void tambahAnak(anak *);
+    void cetakAnak();
+};
+
