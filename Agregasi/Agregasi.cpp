2c62af0162e56cb8963d41df288e954cbe58ea16
aryatama2004 <aryatama261@gmail.com>
2025-05-27 15:21:50 +0700

    membuat objek ibu

--- Agregasi/Agregasi.cpp
+++ Agregasi/Agregasi.cpp
@@ -6,7 +6,11 @@ using namespace std;
 #include "ibu.h"
 
 int main (){
-
+    ibu* varIbu1 = new ibu("dini");
+    ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("dewi");
 
  
 
