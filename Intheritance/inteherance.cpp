3f3167ccf17eaf492a7f73494e21ba8d3ef4d858
aryatama2004 <aryatama261@gmail.com>
2025-05-27 16:23:10 +0700

    memasukkan daftar dokter dan kehadiran pasien

--- Intheritance/inteherance.cpp
+++ Intheritance/inteherance.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+
+class dokter;
+class pasien { //asosiasi
+public :
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "pasien \"" << nama <<  "\" tidak ada\n";
+    }
+    void tambahDokter(dokter*);
+    void cetakDokter();    
+};
